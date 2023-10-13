6328e915ef94b24a647e515e9152ac6a60d251f7
Fareed <fareedsayed95@gmail.com>
2023-10-13 21:31:14 +0530

    First commit

---
+++ tuto12/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index (request):
+   return  render(request,'index.html')
+def function1(request):
+   text = (request.POST.get('text','default'))
+   fullcaps = (request.POST.get('upper','off'))
+   fulllows = (request.POST.get('lower','off'))
+   print(f"Entered text in bar : {text}")
+   print(f"fulcaps :{fullcaps}")
+   print(f"fullows :{fulllows}")
+   if (fullcaps == fulllows == "on"):
+     return render(request,'error.html')
+   if (fullcaps == "on"):
+    cap = text.upper()
+    params = {'YOURINPUT':text,'UPPER':cap}
+    return render(request,'capi.html',params)
+   if (fulllows == "on"):
+    low = text.lower()
+    params = {'YOURINPUT':text,'UPPER':low}
+    return render(request,'capi.html',params)
+   else :
+     return HttpResponse('error')
